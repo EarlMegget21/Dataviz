4b5643c91fda356d35ef692114c7a443e524a794
Rudy <rudy.sonetti@gmail.com>
2017-11-10 18:27:01 +0100

    Map

--- view/view.php
+++ view/view.php
@@ -3,6 +3,13 @@
     <head>
         <meta charset="UTF-8">
         <title><?php echo $pagetitle; ?></title>
+         <!-- CSS temporel pour redimensionner la map -->
+        <style>
+	       #map {
+	        height: 400px;
+	        width: 100%;
+	       }
+		</style>
     </head>
     <body>
         <header>
@@ -11,13 +18,37 @@
                 <a href="index.php?action=readAll&controller=admin">Admin</a>
             </nav>
         </header>
-<?php
-// Si $controller='event' et $view='ListEvent',
-// alors $filepath="/chemin_du_site/view/event/list.php"
-$filepath = File::build_path(array("view", $object,
-    "$view.php"));
-require $filepath;
-?>
+        
+	        <div><?php
+					// Si $controller='event' et $view='ListEvent',
+					// alors $filepath="/chemin_du_site/view/event/list.php"
+					$filepath = File::build_path(array("view", $object, "$view.php"));
+					require $filepath;
+				?>
+			</div>
+
+			<div id="map">
+				<!-- affiche la map ici -->
+			</div>
+			
+		    <script>
+		      function initMap() {
+		        var centre = {lat: -25.363, lng: 131.044};
+		        var map = new google.maps.Map(document.getElementById('map'), { //créer une map
+		          zoom: 4,
+		          center: centre
+		        });
+		        var point1 = new google.maps.Marker({ //créer un point
+		          position: centre,
+		          map: map
+		        });
+		      }
+		    </script>
+		    
+		    <script async defer
+				src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCuog5LlTmtUH8-wB5IjxdJMY_Cq-CqhVU&callback=initMap"> <!-- include l'API Javascript grâce à notre Clé -->
+		    </script>
+
         <footer>
             <p style="border: 1px solid black;text-align:right;padding-right:1em;">Copyright</p>
         </footer>
